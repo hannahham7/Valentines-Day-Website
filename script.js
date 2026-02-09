0cf12ea7bf92d1834b16169faf85a3d3b61e2630
Hannah Ham <hamhannah0116@gmail.com>
2026-02-08 22:36:14 -0500

    Complete question.html and yes.html

--- script.js
+++ script.js
@@ -1,8 +1,49 @@
+const unopenedEnvelope = document.getElementById('unopened-envelope');
+
+const noButton = document.getElementById('no-button');
+const yesButton = document.getElementById('yes-button');
+const questionGif = document.getElementById('yes-hamster');
+
+const originalGifSrc = 'images/yes_hamster.gif';
+
+let messages = ["Are you sure?", "Really sure?", "Are you positive?", "Pookie please...", "Don't make me sad...", "I will be very sad..."]
+let messageIndex = 0;
+
 function openEnvelope() {
     document.querySelector('.envelope').disabled = true;
 
-    const unopenedEnvelope = document.getElementById('unopened-envelope');
     unopenedEnvelope.src = 'images/opened_envelope.png';
 
     setTimeout(() => {window.location.href = 'question.html';}, 1000);
 }
+
+function handleYesClick() {
+    window.location.href = 'yes.html';
+}
+
+function handleNoClick() {
+    noButton.textContent = messages[messageIndex];
+    messageIndex = (messageIndex + 1) % messages.length;
+    const currentSize = parseFloat(window.getComputedStyle(yesButton).fontSize);
+    yesButton.style.fontSize = (currentSize *1.5) + 'px';
+}
+
+noButton.addEventListener("mouseover", () => {
+    questionGif.src = 'images/mad_hamster.gif';
+    questionGif.alt = 'Mad Hamster GIF';
+});
+
+noButton.addEventListener("mouseout", () => {
+    questionGif.src = originalGifSrc;
+    questionGif.alt = 'Yes Hamster GIF';
+});
+
+yesButton.addEventListener("mouseover", () => {
+    questionGif.src = 'images/excited_hamster.gif';
+    questionGif.alt = 'Excited Hamster GIF';
+});
+
+yesButton.addEventListener("mouseout", () => {
+    questionGif.src = originalGifSrc;
+    questionGif.alt = 'Yes Hamster GIF';
+});
