22a239f84bfcf608f3fe25e132644715699283d8
Hannah Ham <hamhannah0116@gmail.com>
2026-02-09 00:08:37 -0500

    Change src files to links

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ const noButton = document.getElementById('no-button');
 const yesButton = document.getElementById('yes-button');
 const questionGif = document.getElementById('please-hamster');
 
-const originalGifSrc = 'images/please_hamster.gif';
+const originalGifSrc = 'https://media1.giphy.com/media/v1.Y2lkPTc5MGI3NjExa25xbm1hZjNwaXhoOGVvMTh4bHp6OW00aWpiaHVpdXV2aHJ4czEzaCZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/QyyK3h1DMaZOuJbYWo/giphy.gif';
 
 let messages = ["Are you sure?", "Really sure?", "Are you positive?", "Pookie please...", "Don't make me sad...", "I will be very sad...", "Pretty please?"]
 let messageIndex = 0;
@@ -29,7 +29,7 @@ function handleNoClick() {
 }
 
 noButton.addEventListener("mouseover", () => {
-    questionGif.src = 'images/mad_hamster.gif';
+    questionGif.src = 'https://media2.giphy.com/media/v1.Y2lkPTc5MGI3NjExM3U4eWJpZHh0Y2VpdzU3aTFmaWNldzljc3phZzI1bDFsZ3NrcDRzcSZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/gUNUi05afFLWm1hYcs/giphy.gif';
     questionGif.alt = 'Mad Hamster GIF';
 });
 
@@ -39,7 +39,7 @@ noButton.addEventListener("mouseout", () => {
 });
 
 yesButton.addEventListener("mouseover", () => {
-    questionGif.src = 'images/excited_hamster.gif';
+    questionGif.src = 'https://media1.giphy.com/media/v1.Y2lkPTc5MGI3NjExbnRzMTR3dHZ1b3E4MXJienI2dzd0bHMxOHh3eDJ5eTdoNG1wbWdnMiZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/MguQrhEC4v6B1viXk1/giphy.gif';
     questionGif.alt = 'Excited Hamster GIF';
 });
 
